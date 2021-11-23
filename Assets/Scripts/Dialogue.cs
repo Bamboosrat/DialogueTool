ee85a702af2eaaa018ccccedaa156c85dde562c4
Bamboosrat <mnguyen@gmx.ch>
2021-11-23 15:42:53 +0100

    Test

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CreateAssetMenu(fileName = "new Dialogue", menuName = "Dialogue", order = 0)]
+    public class Dialogue : ScriptableObject
+    {
+        [SerializeField] private DialogueNode[] nodes;
+
+    }
+}
